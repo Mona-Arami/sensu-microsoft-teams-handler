759333ae8f9da2a4b8a5e678211f96eff5368142
Arami <marami@contractor.usgs.gov>
2023-08-02 09:56:54 -0600

    modify bin file

--- src/bin/microsoft_teams.py
+++ src/bin/microsoft_teams.py
@@ -1,16 +1,16 @@
-echo $PWD
+
 #/usr/bin/env bash
 # Absolute path to this script. /home/user/bin/foo.sh
 SCRIPT=$(readlink -f $0)
-echo SCRIPT
+echo "SCRIPT=$(readlink -f $0)"
 # Absolute path this script is in. /home/user/bin
 SCRIPTPATH=`dirname $SCRIPT`
-echo SCRIPTPATH
+echo "SCRIPTPATH=`dirname $SCRIPT`"
 asset_path=$(dirname -- ${SCRIPTPATH})
-echo asset_path
+echo "asset_path=$(dirname -- ${SCRIPTPATH})"
 # Prepend relative library path to PYTHONPATH
 # to ensure modules are found.
 export PYTHONPATH="${asset_path}/lib:$PYTHONPATH"
-echo PYTHONPATH
+echo "PYTHONPATH="${asset_path}/lib:$PYTHONPATH""
 program="${0##*/}"
 exec "${asset_path}/libexec/${program}" "$@"
