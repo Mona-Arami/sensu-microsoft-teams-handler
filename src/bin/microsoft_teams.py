f3d9ee966d239366c47cad469134073471ead6a6
Arami <marami@contractor.usgs.gov>
2023-08-09 09:24:40 -0600

    removed pythonpath

--- src/bin/microsoft_teams.py
+++ src/bin/microsoft_teams.py
@@ -5,10 +5,11 @@ SCRIPT=$(readlink -f $0)
 # Absolute path this script is in. /home/user/bin
 SCRIPTPATH=`dirname $SCRIPT`
 asset_path=$(dirname -- ${SCRIPTPATH})
+echo "$(dirname -- ${SCRIPTPATH})"
 
 # Prepend relative library path to PYTHONPATH
 # to ensure modules are found.
-export PYTHONPATH="${asset_path}/lib:$PYTHONPATH"
-echo "PYTHONPATH="${asset_path}/lib:$PYTHONPATH""
+# export PYTHONPATH="${asset_path}/lib:$PYTHONPATH"
+# echo "PYTHONPATH="${asset_path}/lib:$PYTHONPATH""
 program="${0##*/}"
 exec "${asset_path}/libexec/${program}" "$@"
